2c57f8b863145698d670520c725ea289175e8cd4
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-06-22 12:57:46 -0600

    Update reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
-import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { StatisticsReducer } from './stats'
   
   const rootReducer = combineReducers({
-  
+    StatisticsReducer,
   });
   
-  const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+  const store = configureStore(rootReducer, applyMiddleware(logger, thunk));
   
   export default store;
   
